a0a3235287d05fb7a3113f3a7a567a820aa5d139
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2024-02-12 10:26:00 -0800

    first commit

---
+++ gptlabs/utils/train.py
@@ -0,0 +1,22 @@
+import torch
+from gptlabs.optimizers.adamw import AdamW
+from gptlabs.trainers.trainer import Trainer
+from data.text_data import TextBatchGenerator
+from torch import nn
+
+
+
+def generate(model: nn.Module, context: torch.Tensor, tokenizer, max_new_tokens: int = 500):
+    print("Generating text...")
+    model.eval()  # Ensure the model is in evaluation mode
+    with torch.no_grad():
+        generated_tokens = model.generate(context, max_new_tokens=max_new_tokens)[0].tolist()
+    return tokenizer.decode(generated_tokens)
+
+def train_model(model: nn.Module, hyperparameters: dict, batch_generator: TextBatchGenerator):
+    print("Training the model...")
+    optimizer = AdamW(model.parameters(), lr=hyperparameters["learning_rate"])
+    loss_fn = torch.nn.functional.cross_entropy
+    trainer = Trainer(data=batch_generator, model=model, optimizer=optimizer, loss_fn=loss_fn)
+    trainer.train(hyperparameters["batch_size"], hyperparameters["max_iters"], hyperparameters["eval_interval"])
+
